e4c5c38365cba983cb68339697d171c31e3dc533
hardik singh <realhardik18@gmail.com>
2024-12-18 20:03:04 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function App() {
   const targetDate = new Date('2024-12-19T09:00:00'); // Set target date and time
@@ -107,7 +107,6 @@ function App() {
   }, [timeLeft]);
 
   // Convert time left into days, hours, minutes, and seconds
-  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
   const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
