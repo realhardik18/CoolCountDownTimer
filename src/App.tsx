21a4aefd2b2b17495dfc82df09a796b24d67c1aa
hardik singh <realhardik18@gmail.com>
2024-12-18 19:56:36 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -105,7 +105,6 @@ function App() {
   }, [timeLeft]);
 
   // Convert time left into days, hours, minutes, and seconds
-  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
   const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
